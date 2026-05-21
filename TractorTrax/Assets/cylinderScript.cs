ca94c648926105f4616db897da1858eee2d32263
metalDogFish <58714411+metalDogFish@users.noreply.github.com>
2026-05-21 16:02:08 -0400

    script test

---
+++ TractorTrax/Assets/cylinderScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cylinderScript : MonoBehaviour {
+
+	private Transform trans;
+	// Use this for initialization
+	void Start () {
+		trans = GetComponent<Transform>();
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		trans.Rotate(0.3f, 0, 1.0f);
+	}
+}
